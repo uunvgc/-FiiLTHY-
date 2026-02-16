b6f479e68a3027db596912181f1d50a9f4a9a84f
uunvgc <Keithross1205@gmail.com>
2026-02-16 11:19:08 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Create React root and render app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
