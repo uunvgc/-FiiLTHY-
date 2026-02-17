4911258b338e0ba57c7c9e7cb5d99944cd87fb39
uunvgc <Keithross1205@gmail.com>
2026-02-16 16:34:22 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import Dashboard from "./components/Dashboard";
 
 export default function App() {
-  return (
-    <div style={{ background: "red", minHeight: "100vh", padding: 20 }}>
-      <h1 style={{ color: "white" }}>APP UPDATED ✅</h1>
-      <Dashboard />
-    </div>
-  );
+  return <Dashboard />;
 }
