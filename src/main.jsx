66937e34213d1375602ea1e45cec4d11c2292654
uunvgc <Keithross1205@gmail.com>
2026-02-27 01:32:01 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import "./index.css"; // or "./styles.css" if that's what you use
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ErrorBoundary>
