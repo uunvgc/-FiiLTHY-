4e3398fcee7b37eda2104cb1632c06a751887f02
uunvgc <Keithross1205@gmail.com>
2026-02-27 17:30:36 -0800

    Update Leads.jsx

--- src/pages/Leads.jsx
+++ src/pages/Leads.jsx
@@ -112,7 +112,15 @@ export default function Leads() {
 
       <div className="stack" style={{ marginTop: 12 }}>
         {filtered.map((lead) => (
-          <LeadCard key={lead.id || lead.source_id || Math.random()} lead={lead} />
+          <LeadCard
+            key={
+              lead.id ||
+              lead.source_id ||
+              lead.permalink ||
+              `${lead.source}-${lead.author}-${lead.posted_at}`
+            }
+            lead={lead}
+          />
         ))}
       </div>
 
